7f71c6798231aaa17a2c2129d7c512ef58c54e60
liujinzhu <liujinzhu@xiaohongshu.net>
2024-09-14 17:22:17 +0800

    整理

--- assets/js/love.js
+++ assets/js/love.js
@@ -1,13 +1,32 @@
 // 定义
 var canvas = document.getElementById("love-canvas");
 var ctx = canvas.getContext("2d");
-var width = canvas.width;
-var height = canvas.height;
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+var width = window.innerWidth;
+var height = window.innerHeight;
 
-// 绘制气球
-function draw() {
+// 数据绘制
+var drawList = []
+// 文本绘制
+function drawText(obj) {
+  ctx.font = obj.font;
+  ctx.fillStyle = obj.color;
+  ctx.fillText(obj.text, obj.x, obj.y);
 }
-draw()
+function drawData(obj) {
+  /** 数据设置 */
+  /** 清空画布 */
+  /** 绘制数据 */
+  ctx.beginPath();
+}
+drawText({
+  text: "I love you",
+  x: 10,
+  y: 20,
+  font: "12px",
+  color: "red"
+})
 
 document.addEventListener('click', function(event) {
   const box = document.createElement('div');
