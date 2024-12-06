2015ddb0ae8e1af4f3e350d688463c13898c5de2
liujinzhu <liujinzhu@xiaohongshu.net>
2024-12-06 21:17:22 +0800

    feat: 路线

---
+++ assets/data/frontEnd.js
@@ -0,0 +1,443 @@
+var frontEnd = [
+  {
+    id: "VlNNwIEDWqQXtqkHWJYzC",
+    title: "Internet",
+    subtopics: [
+      {
+        id: "yCnn-NfSxIybUQ2iTuUGq",
+        parentId: "VlNNwIEDWqQXtqkHWJYzC",
+        title: "How does the internet work?",
+      },
+      {
+        id: "R12sArWVpbIs_PHxBqVaR",
+        parentId: "VlNNwIEDWqQXtqkHWJYzC",
+        title: "What is HTTP?",
+      },
+      {
+        id: "ZhSuu2VArnzPDp6dPQQSC",
+        parentId: "VlNNwIEDWqQXtqkHWJYzC",
+        title: "What is Domain Name?",
+      },
+      {
+        id: "aqMaEY8gkKMikiqleV5EP",
+        parentId: "VlNNwIEDWqQXtqkHWJYzC",
+        title: "What is hosting?",
+      },
+      {
+        id: "hkxw9jPGYphmjhTjw8766",
+        parentId: "VlNNwIEDWqQXtqkHWJYzC",
+        title: "DNS and how it works?",
+      },
+      {
+        id: "P82WFaTPgQEPNp5IIuZ1Y",
+        parentId: "VlNNwIEDWqQXtqkHWJYzC",
+        title: "Browsers and how they work?",
+      },
+    ],
+  },
+  {
+    id: "yWG2VUkaF5IJVVut6AiSy",
+    title: "HTML",
+    subtopics: [
+      { id: "PCirR2QiFYO89Fm-Ev3o1", title: "Learn the basics" },
+      { id: "z8-556o-PaHXjlytrawaF", title: "Writing Semantic HTML" },
+      { id: "V5zucKEHnIPPjwHqsMPHF", title: "Forms and Validations" },
+      { id: "iJIqi7ngpGHWAqtgdjgxB", title: "Accessibility" },
+      {
+        id: "mH_qff8R7R6eLQ1tPHLgG",
+        parentId: "yWG2VUkaF5IJVVut6AiSy",
+        title: "SEO Basics",
+      },
+    ],
+  },
+  {
+    id: "ZhJhf1M2OphYbEmduFq-9",
+    title: "CSS",
+    subtopics: [
+      {
+        id: "YFjzPKWDwzrgk2HUX952L",
+        parentId: "ZhJhf1M2OphYbEmduFq-9",
+        title: "Learn the basics",
+      },
+      {
+        id: "dXeYVMXv-3MRQ1ovOUuJW",
+        parentId: "ZhJhf1M2OphYbEmduFq-9",
+        title: "Making Layouts",
+      },
+      {
+        id: "TKtWmArHn7elXRJdG6lDQ",
+        parentId: "ZhJhf1M2OphYbEmduFq-9",
+        title: "Responsive Design",
+      },
+    ],
+  },
+  {
+    id: "ODcfFEorkfJNupoQygM53",
+    title: "JavaScript",
+    subtopics: [
+      {
+        id: "A4brX0efjZ0FFPTB4r6U0",
+        parentId: "ODcfFEorkfJNupoQygM53",
+        title: "Fetch API / Ajax (XHR)",
+      },
+      {
+        id: "0MAogsAID9R04R5TTO2Qa",
+        parentId: "ODcfFEorkfJNupoQygM53",
+        title: "Learn DOM Manipulation",
+      },
+      {
+        id: "wQSjQqwKHfn5RGPk34BWI",
+        parentId: "ODcfFEorkfJNupoQygM53",
+        title: "Learn the Basics",
+      },
+    ],
+  },
+  {
+    id: "NIY7c4TQEEHx0hATu-k5C",
+    title: "Version Control Systems",
+    subtopics: [
+      {
+        id: "R_I4SGYqLk5zze5I1zS_E",
+        parentId: "NIY7c4TQEEHx0hATu-k5C",
+        title: "Git",
+      },
+    ],
+  },
+  {
+    id: "MXnFhZlNB1zTsBFDyni9H",
+    title: "VCS Hosting",
+    subtopics: [
+      { id: "qmTVMJDsEhNIkiwE_UTYu", title: "GitHub" },
+      { id: "zIoSJMX3cuzCgDYHjgbEh", title: "GitLab" },
+      {
+        id: "DILBiQp7WWgSZ5hhtDW6A",
+        parentId: "MXnFhZlNB1zTsBFDyni9H",
+        title: "Bitbucket",
+      },
+    ],
+  },
+  {
+    id: "IqvS1V-98cxko3e9sBQgP",
+    title: "Package Managers",
+    subtopics: [
+      {
+        id: "yrq3nOwFREzl-9EKnpU-e",
+        parentId: "IqvS1V-98cxko3e9sBQgP",
+        title: "yarn",
+      },
+      {
+        id: "SLxA5qJFp_28TRzr1BjxZ",
+        parentId: "IqvS1V-98cxko3e9sBQgP",
+        title: "pnpm",
+      },
+      {
+        id: "ib_FHinhrw8VuSet-xMF7",
+        parentId: "IqvS1V-98cxko3e9sBQgP",
+        title: "npm",
+      },
+    ],
+  },
+  {
+    id: "eXezX7CVNyC1RuyU_I4yP",
+    title: "Pick a Framework",
+    subtopics: [
+      {
+        id: "-bHFIiXnoUQSov64WI9yo",
+        parentId: "eXezX7CVNyC1RuyU_I4yP",
+        title: "Angular",
+      },
+      {
+        id: "ERAdwL1G9M1bnx-fOm5ZA",
+        parentId: "eXezX7CVNyC1RuyU_I4yP",
+        title: "Vue.js",
+      },
+      {
+        id: "tG5v3O4lNIFc2uCnacPak",
+        parentId: "eXezX7CVNyC1RuyU_I4yP",
+        title: "React",
+      },
+      {
+        id: "ZR-qZ2Lcbu3FtqaMd3wM4",
+        parentId: "eXezX7CVNyC1RuyU_I4yP",
+        title: "Svelte",
+      },
+      {
+        id: "DxOSKnqAjZOPP-dq_U7oP",
+        parentId: "eXezX7CVNyC1RuyU_I4yP",
+        title: "Solid JS",
+      },
+      {
+        id: "N5DCb6bDfgUnSdHPLYY4g",
+        parentId: "eXezX7CVNyC1RuyU_I4yP",
+        title: "Qwik",
+      },
+    ],
+  },
+  {
+    id: "XDTD8el6OwuQ55wC-X4iV",
+    title: "Writing CSS",
+    subtopics: [{ id: "eghnfG4p7i-EDWfp3CQXC", title: "Tailwind" }],
+  },
+  { id: "nPg_YWpMJtlhU2t2UD_6B", title: "CSS Architecture", subtopics: [
+    { id: "dRDmS072xeNLX7p_X565w", title: "BEM" },
+  ] },
+  {
+    id: "UTW1pP59dUehuf0zeHXqL",
+    title: "CSS Preprocessors",
+    subtopics: [
+      { id: "kukEE5rMSPa4NeNjx21kt", title: "Sass" },
+      { id: "9WlPENh9g1xOv-zA64Tfg", title: "PostCSS" },
+    ],
+  },
+  { id: "i9z0stM4uKu27Cz6NIgNX", title: "Build Tools", subtopics: [
+    {
+      id: "9VcGfDBBD8YcKatj4VcH1",
+      title: "Linters and Formatters",
+      subtopics: [
+        { id: "zbkpu_gvQ4mgCiZKzS1xv", title: "Prettier" },
+        { id: "NFjsI712_qP0IOmjuqXar", title: "ESLint" },
+      ],
+    },
+    {
+      id: "hkSc_1x09m7-7BO7WzlDT",
+      title: "Module Bundlers",
+      subtopics: [
+        {
+          id: "NS-hwaWa5ebSmNNRoxFDp",
+          parentId: "hkSc_1x09m7-7BO7WzlDT",
+          title: "Parcel",
+        },
+        {
+          id: "sCjErk7rfWAUvhl8Kfm3n",
+          parentId: "hkSc_1x09m7-7BO7WzlDT",
+          title: "Rollup",
+        },
+        {
+          id: "twufEtHgxcRUWAUQ9bXus",
+          parentId: "hkSc_1x09m7-7BO7WzlDT",
+          title: "Webpack",
+        },
+        {
+          id: "4W7UXfdKIUsm1bUrjdTVT",
+          parentId: "hkSc_1x09m7-7BO7WzlDT",
+          title: "esbuild",
+        },
+        {
+          id: "0Awx3zEI5_gYEIrD7IVX6",
+          parentId: "hkSc_1x09m7-7BO7WzlDT",
+          title: "Vite",
+        },
+        { id: "h26uS3muFCabe6ekElZcI", title: "SWC" },
+      ],
+    },
+
+  ] },
+  {
+    id: "igg4_hb3XE3vuvY8ufV-4",
+    title: "Testing",
+    subtopics: [
+      { id: "hVQ89f6G0LXEgHIOKHDYq", title: "Vitest" },
+      {
+        id: "g5itUjgRXd9vs9ujHezFl",
+        parentId: "igg4_hb3XE3vuvY8ufV-4",
+        title: "Jest",
+      },
+      {
+        id: "jramLk8FGuaEH4YpHIyZT",
+        parentId: "igg4_hb3XE3vuvY8ufV-4",
+        title: "Playwright",
+      },
+      { id: "DaynCz5RR26gjT6N6gTDL", title: "Cypress" },
+    ],
+  },
+  {
+    id: "U5mD5FmVx7VWeKxDpQxB5",
+    title: "Authentication Strategies",
+    subtopics: ['JWT', 'OAuth', 'SSO', 'Basic Auth', 'Session Auth'],
+  },
+  {
+    id: "RDWbG3Iui6IPgp0shvXtg",
+    title: "Web Security Basics",
+    subtopics: [
+      { id: "AfH2zCbqzw0Nisg1yyISS", title: "CORS" },
+      { id: "uum7vOhOUR38vLuGZy8Oa", title: "HTTPS" },
+      { id: "rmcm0CZbtNVC9LZ14-H6h", title: "Content Security Policy" },
+      {
+        id: "JanR7I_lNnUCXhCMGLdn-",
+        parentId: "RDWbG3Iui6IPgp0shvXtg",
+        title: "OWASP Security Risks",
+      },
+    ],
+  },
+  {
+    id: "ruoFa3M4bUE3Dg6GXSiUI",
+    title: "Web Components",
+    subtopics: [
+      {
+        id: "Hk8AVonOd693_y1sykPqd",
+        parentId: "ruoFa3M4bUE3Dg6GXSiUI",
+        title: "HTML Templates",
+      },
+      { id: "VxiQPgcYDFAT6WgSRWpIA", title: "Custom Elements" },
+      { id: "-SpsNeOZBkQfDA-rwzgPg", title: "Shadow DOM" },
+    ],
+  },
+  {
+    id: "hwPOGT0-duy3KfI8QaEwF",
+    title: "Type Checkers",
+    subtopics: [
+      {
+        id: "0asdhvwBH3gn-ercktV7A",
+        parentId: "hwPOGT0-duy3KfI8QaEwF",
+        title: "TypeScript",
+      },
+    ],
+  },
+  {
+    id: "Cxspmb14_0i1tfw-ZLxEu",
+    title: "SSR",
+    subtopics: [
+      {
+        id: "SGDf_rbfmFSHlxI-Czzlz",
+        parentId: "Cxspmb14_0i1tfw-ZLxEu",
+        title: "React",
+        subtopics: [
+          {
+            id: "zNFYAJaSq0YZXL5Rpx1NX",
+            parentId: "SGDf_rbfmFSHlxI-Czzlz",
+            title: "Next.js",
+          },
+          { id: "iUxXq7beg55y76dkwhM13", title: "Astro" },
+          {
+            id: "KJRkrFZIihCUBrOf579EU",
+            parentId: "SGDf_rbfmFSHlxI-Czzlz",
+            title: "react-router",
+          },
+        ],
+      },
+      {
+        id: "k6rp6Ua9qUEW_DA_fOg5u",
+        parentId: "Cxspmb14_0i1tfw-ZLxEu",
+        title: "Angular",
+      },
+      {
+        id: "3TE_iYvbklXK0be-5f2M7",
+        parentId: "Cxspmb14_0i1tfw-ZLxEu",
+        title: "Vue.js",
+        subtopics: [
+          {
+            id: "BBsXxkbbEG-gnbM1xXKrj",
+            parentId: "3TE_iYvbklXK0be-5f2M7",
+            title: "Nuxt.js",
+          },
+        ],
+      },
+      {
+        id: "OL8I6nOZ8hGGWmtxg_Mv8",
+        parentId: "Cxspmb14_0i1tfw-ZLxEu",
+        title: "Svelte",
+        subtopics: [
+          {
+            id: "P4st_telfCwKLSAU2WsQP",
+            parentId: "OL8I6nOZ8hGGWmtxg_Mv8",
+            title: "Svelte Kit",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: "L7AllJfKvClaam3y-u6DP",
+    title: "GraphQL",
+    subtopics: [
+      { id: "5eUbDdOTOfaOhUlZAmmXW", title: "Apollo" },
+      {
+        id: "0moPO23ol33WsjVXSpTGf",
+        parentId: "L7AllJfKvClaam3y-u6DP",
+        title: "Relay Modern",
+      },
+    ],
+  },
+  {
+    id: "PoM77O2OtxPELxfrW1wtl",
+    title: "PWAs",
+    subtopics: [
+      {
+        id: "3_sJHKTogkDoCjR518-OL",
+        parentId: "PoM77O2OtxPELxfrW1wtl",
+        title: "Using DevTools",
+      },
+      {
+        id: "raoa-75p_DyBAycvy3yVv",
+        parentId: "PoM77O2OtxPELxfrW1wtl",
+        title: "Storage",
+      },
+      { id: "-DsETM9xLgHyGZthptj1Y", title: "PRPL Pattern" },
+      { id: "xD5WfEP7Ez0oi3890UgmH", title: "RAIL Model" },
+      { id: "X0Y3-IpPiFUCsNDK4RFxw", title: "Performance Metrics" },
+      { id: "RIhHMHLsLLPhNl05Q9aBf", title: "Using Lighthouse" },
+      { id: "NDJR8UCoa31v45TBFP7we", title: "Web Sockets" },
+      { id: "doPe92aUpo-8KWhi45lWK", title: "Server Sent Events" },
+      { id: "TldWoXiqKxM4X3JONKAR7", title: "Service Workers" },
+      { id: "YbGGYoKJEx29PlvopUBiM", title: "Location" },
+      { id: "6AlcArOiJMhHXguAosDzn", title: "Notifications" },
+      { id: "Fd0hQh1DleM0gMzCpGou4", title: "Device Orientation" },
+      { id: "MAM1nuVk-h4AvTUk4nvmj", title: "Payments" },
+      { id: "opu2bAsmdWHqWqtsCscLC", title: "Credentials" },
+    ],
+  },
+  {
+    id: "n0q32YhWEIAUwbGXexoqV",
+    title: "Static Site Generators",
+    subtopics: [
+      { id: "CMrss8E2W0eA6DVEqtPjT", title: "Vuepress" },
+      {
+        id: "XWJxV42Dpu2D3xDK10Pn3",
+        parentId: "n0q32YhWEIAUwbGXexoqV",
+        title: "Nuxt.js",
+      },
+      {
+        id: "io0RHJWIcVxDhcYkV9d38",
+        parentId: "n0q32YhWEIAUwbGXexoqV",
+        title: "Eleventy",
+      },
+      { id: "V70884VcuXkfrfHyLGtUg", title: "Next.js" },
+      {
+        id: "wA2fSYsbBYU02VJXAvUz8",
+        parentId: "SGDf_rbfmFSHlxI-Czzlz",
+        title: "Astro",
+      },
+    ],
+  },
+  {
+    id: "VOGKiG2EZVfCBAaa7Df0W",
+    title: "Mobile Apps",
+    subtopics: [
+      { id: "dsTegXTyupjS8iU6I7Xiv", title: "React Native" },
+      { id: "dIQXjFEUAJAGxxfAYceHU", title: "Flutter" },
+      {
+        id: "xmRv6-L45m5MDpHmdHFCL",
+        parentId: "VOGKiG2EZVfCBAaa7Df0W",
+        title: "Ionic",
+      },
+    ],
+  },
+  {
+    id: "KMA7NkxFbPoUDtFnGBFnj",
+    title: "Desktop Apps",
+    subtopics: [
+      { id: "mQHpSyMR4Rra4mqAslgiS", title: "Electron" },
+      {
+        id: "GJctl0tVXe4B70s35RkLT",
+        parentId: "KMA7NkxFbPoUDtFnGBFnj",
+        title: "Tauri",
+      },
+      {
+        id: "2MRvAK9G9RGM_auWytcKh",
+        parentId: "KMA7NkxFbPoUDtFnGBFnj",
+        title: "Flutter",
+      },
+      
+    ],
+  },
+];
