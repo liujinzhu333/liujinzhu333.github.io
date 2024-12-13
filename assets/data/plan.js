11b1716daa55fd6df80eeef45302bd7ae8477402
liujinzhu <liujinzhu@xiaohongshu.net>
2024-12-13 17:59:38 +0800

    基本

--- assets/data/plan.js
+++ assets/data/plan.js
@@ -1,7 +1,42 @@
-export const plan = [
+export const planList = [
   {
-    title: 'Plan 1',
-    description: 'This is plan 1',
-    status: 1,
+    name: '学习前端技术',
+    title: '',
+    children: [],
+    status: 0,
+  },
+  {
+    name: 'plan组件添加状态',
+    title: '',
+    children: [],
+    status: 0,
+  },
+  {
+    name: 'plan组件添加type属性',
+    title: '',
+    children: [],
+    status: 0,
+  },
+  {
+    name: '整理历史数据',
+    title: '',
+    children: [],
+    status: 0,
   }
+]
+// 项目计划
+export const projectPlan = [
+  {
+    name: '单页简历（CV）',
+    title: '',
+    children: [],
+    status: 0,
+  },
+  {
+    name: '基本HTML网站',
+    title: '',
+    children: [],
+    status: 0,
+  }
+
 ]
