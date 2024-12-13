8b7ac53526805e823ed06a042e4b1f06cf6a1f3b
liujinzhu <liujinzhu@xiaohongshu.net>
2024-12-13 17:24:49 +0800

    修改

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,20 @@
-(function init(){
+function hello() {
   const dom = document.getElementById('hello');
   if (!dom) return;
   // const time = new Date().toLocaleDateString();
   const hour = new Date().getHours();
+  let timeText = ''
   if (hour < 6 || hour >= 20) {
-    dom.innerHTML = `深夜好`;
+    timeText = `深夜好！`;
   } else if (hour < 12) {
-    dom.innerHTML = `早上好！`
+    timeText = `早上好！`
   } else if (hour < 18) {
-    dom.innerHTML = `下午好`;
+    timeText = `下午好！`;
   } else {
-    dom.innerHTML = `晚上好`;
+    timeText = `晚上好！`;
   }
+  dom.innerHTML = `${timeText} 今天是 ${new Date().toLocaleDateString()}。`;
+}
+(function init(){
+  hello()
 })()
